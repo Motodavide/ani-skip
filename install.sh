35671479b14e11873f8e83b4a4ed12e76aa4cc17
Davide Costa <davide.costa@davcloud.xyz>
2024-10-08 19:30:09 +0200

    Better conditions

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 install -D ./skip.lua ~/.config/mpv/scripts/skip.lua
 echo "Installing in /usr/bin"
-[ -z $(ls /usr/bin/doas) ] || doas install ./ani-skip /usr/bin/ani-skip
-[ -z $(ls /usr/bin/sudo) ] || sudo install ./ani-skip /usr/bin/ani-skip
+[ -f "/usr/bin/doas" ] && doas install ./ani-skip /usr/bin/ani-skip
+[ -f "/usr/bin/sudo" ] && sudo install ./ani-skip /usr/bin/ani-skip
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 rm $HOME/.config/mpv/scripts/skip.lua
 echo "Uninstalling from /usr/bin"
-[ -z $(ls /usr/bin/doas) ] || doas rm /usr/bin/ani-skip
-[ -z $(ls /usr/bin/sudo) ] || sudo rm /usr/bin/ani-skip
+[ -f "/usr/bin/doas" ] && doas rm /usr/bin/ani-skip
+[ -f "/usr/bin/sudo" ] && sudo rm /usr/bin/ani-skip
